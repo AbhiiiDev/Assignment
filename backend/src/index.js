d1ec050e369675fb13fe098d273115a8d1afcfba
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-05-12 20:35:39 +0530

    making backend deploy ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,11 @@ const app=express();
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173' }));
 
+app.get('/',(req,res)=>
+{
+    res.send('King, Your backend is working');
+})
+
 
 const calculateRoute=require('./routes/calculateRoutes');
 app.use('/',calculateRoute)
